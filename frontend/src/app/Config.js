64c2326a89670af19d41f7fccea3d9c36546a07a
bittokazi <bitto.kazi@gmail.com>
2022-01-11 19:05:17 +0100

    :sparkles: category update

--- frontend/src/app/Config.js
+++ frontend/src/app/Config.js
@@ -1,9 +1,9 @@
-export const devConfig = {
+const devConfig = {
   API_BASE_URL: "http://localhost:5000/api",
 };
 
-export const prodConfig = {
+const prodConfig = {
   API_BASE_URL: "https://spa-demo.webexperiment.cf/api",
 };
 
-export const Config = devConfig;
+export const Config = prodConfig;
